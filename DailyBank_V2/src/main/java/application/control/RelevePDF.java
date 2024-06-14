e00b2d224d4547eb0b80930f5ac26f5f5f096833
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-06-14 16:56:06 +0200

    releve pdf finishied

--- DailyBank_V2/src/main/java/application/control/RelevePDF.java
+++ DailyBank_V2/src/main/java/application/control/RelevePDF.java
@@ -3,8 +3,8 @@ package application.control;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.Date;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
@@ -27,12 +27,14 @@ import model.orm.exception.DatabaseConnexionException;
 
 public class RelevePDF {
 
-    private static Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+    private static Font catFont = new Font(Font.FontFamily.HELVETICA, 24, Font.BOLD);
+    private static Font smallBold = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+    private static Font normalFont = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL);
+    private static Font tableHeaderFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
 
     public static void generateReleve(int idNumCompte, Client cc, Stage primaryStage) {
         // Récupérer le nom du propriétaire et le numéro du compte
-        String nomProprietaire = "" + cc.nom + "_" + cc.prenom;
+        String nomProprietaire = cc.nom + " " + cc.prenom;
         String nomFichier = "RelevePDF_id_" + idNumCompte + "_" + nomProprietaire + ".pdf";
 
         try {
@@ -50,13 +52,13 @@ public class RelevePDF {
                 PdfWriter.getInstance(document, new FileOutputStream(file));
                 document.open();
                 addMetaData(document, "Relevé de Compte de " + nomProprietaire);
-                addContent(document, idNumCompte);
+                addContent(document, idNumCompte, cc);
                 document.close();
 
                 Alert al = new Alert(AlertType.INFORMATION);
-		        al.setHeaderText("PDF Créé avec succès");
-		        al.show();
-            }else{
+                al.setHeaderText("PDF créé avec succès");
+                al.show();
+            } else {
                 Alert al = new Alert(AlertType.INFORMATION);
                 al.setHeaderText("PDF non sauvegardé");
                 al.show();
@@ -74,48 +76,99 @@ public class RelevePDF {
         document.addCreator("DailyBank");
     }
 
-    private static void addContent(Document document, int idNumCompte) throws DocumentException {
-        Paragraph preface = new Paragraph();
-        addEmptyLine(preface, 1);
-        preface.add(new Paragraph("Relevé de Compte", catFont));
-        addEmptyLine(preface, 1);
-        preface.add(new Paragraph("Report generated for: " + System.getProperty("user.name") + ", " + new Date(), smallBold));
-        addEmptyLine(preface, 2);
+    private static void addContent(Document document, int idNumCompte, Client cc) throws DocumentException {
+        // Titre du document centré
+        Paragraph title = new Paragraph("Relevé de Compte", catFont);
+        title.setAlignment(Element.ALIGN_CENTER);
+        document.add(title);
+
+        addEmptyLine(document, 2);
+
+        // Informations sur le client et le compte
+        document.add(createInformationSection("Nom du propriétaire: ", cc.nom + " " + cc.prenom));
+        document.add(createInformationSection("Adresse: ", cc.adressePostale));
+        document.add(createInformationSection("Numéro de téléphone: ", cc.telephone));
+        document.add(createInformationSection("Email: ", cc.email));
+        document.add(createInformationSection("Numéro du compte: ", String.valueOf(idNumCompte)));
+
+        addEmptyLine(document, 2);
+
+        // Ajouter la section des opérations
+        Paragraph operationsSection = new Paragraph("Opérations sur le compte :", smallBold);
+        document.add(operationsSection);
+        addEmptyLine(document, 1);
 
         // Récupérer les opérations du compte
         ArrayList<Operation> operations = getOperations(idNumCompte);
 
         // Créer la table des opérations
         PdfPTable table = new PdfPTable(4);
+        table.setWidthPercentage(100);
 
-        PdfPCell c1 = new PdfPCell(new Phrase("Date Opération"));
+        // En-têtes de colonnes
+        PdfPCell c1 = new PdfPCell(new Phrase("Date Opération", tableHeaderFont));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        c1.setBackgroundColor(BaseColor.CYAN); // Couleur de fond pour l'en-tête
         table.addCell(c1);
 
-        c1 = new PdfPCell(new Phrase("Date Valeur"));
+        c1 = new PdfPCell(new Phrase("Date Valeur", tableHeaderFont));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        c1.setBackgroundColor(BaseColor.CYAN);
         table.addCell(c1);
 
-        c1 = new PdfPCell(new Phrase("Montant"));
+        c1 = new PdfPCell(new Phrase("Montant", tableHeaderFont));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        c1.setBackgroundColor(BaseColor.CYAN);
         table.addCell(c1);
 
-        c1 = new PdfPCell(new Phrase("Type Opération"));
+        c1 = new PdfPCell(new Phrase("Type Opération", tableHeaderFont));
         c1.setHorizontalAlignment(Element.ALIGN_CENTER);
+        c1.setBackgroundColor(BaseColor.CYAN);
         table.addCell(c1);
 
-        table.setHeaderRows(1);
-
+        // Ajouter les opérations avec alternance de couleurs
+        boolean colorSwitch = false;
         for (Operation op : operations) {
-            table.addCell(op.dateOp.toString());
-            table.addCell(op.dateValeur.toString());
-            table.addCell(String.valueOf(op.montant));
-            table.addCell(op.idTypeOp);
+            PdfPCell dateOpCell = new PdfPCell(new Phrase(op.dateOp.toString(), normalFont));
+            dateOpCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            if (colorSwitch) {
+                dateOpCell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            }
+            table.addCell(dateOpCell);
+
+            PdfPCell dateValeurCell = new PdfPCell(new Phrase(op.dateValeur.toString(), normalFont));
+            dateValeurCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            if (colorSwitch) {
+                dateValeurCell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            }
+            table.addCell(dateValeurCell);
+
+            PdfPCell montantCell = new PdfPCell(new Phrase(String.valueOf(op.montant), normalFont));
+            montantCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            if (colorSwitch) {
+                montantCell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            }
+            table.addCell(montantCell);
+
+            PdfPCell idTypeOpCell = new PdfPCell(new Phrase(op.idTypeOp, normalFont));
+            idTypeOpCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            if (colorSwitch) {
+                idTypeOpCell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+            }
+            table.addCell(idTypeOpCell);
+
+            colorSwitch = !colorSwitch; // Alterner les couleurs des lignes
         }
 
-        preface.add(table);
+        document.add(table);
+    }
 
-        document.add(preface);
+    private static Paragraph createInformationSection(String label, String value) {
+        Paragraph p = new Paragraph();
+        p.add(new Phrase(label, smallBold));
+        p.add(new Phrase(value, normalFont));
+        p.setAlignment(Element.ALIGN_LEFT);
+        return p;
     }
 
     private static ArrayList<Operation> getOperations(int idNumCompte) {
@@ -129,9 +182,9 @@ public class RelevePDF {
         return operations;
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
+    private static void addEmptyLine(Document document, int number) throws DocumentException {
         for (int i = 0; i < number; i++) {
-            paragraph.add(new Paragraph(" "));
+            document.add(new Paragraph(" "));
         }
     }
 }
